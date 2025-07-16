34761bc8debad33c173d88a16db54d361fbf643a
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2025-07-16 11:12:31 +0200

    Updated styles to include preview templates

--- templates/preview-loop.php
+++ templates/preview-loop.php
@@ -10,12 +10,13 @@
  */
 
 $oa_view     = $atts['view'] ?? 'list';
+$oa_class    = sprintf( 'oa-event-%s', sanitize_title( $oa_view ) );
 $oa_template = ! empty( openagenda_get_template( 'preview' ) ) ? openagenda_get_template( 'preview' ) : openagenda_get_template( 'event' );
 
 if ( $openagenda->have_events() ) :
 	$openagenda->reset_index(); ?>
-	<div class="oa-preview <?php echo esc_attr( $oa_view ); ?>">
-		<div class="oa-preview-events">
+	<div class="oa-preview">
+		<div class="oa-preview-events <?php echo esc_attr( $oa_class ); ?>">
 			<?php
 			while ( $openagenda->have_events() ) :
 				$openagenda->the_event();
