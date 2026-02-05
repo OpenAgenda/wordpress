0c1304e1ed7e7a45ce75d627b7cad51c54df06a3
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2026-02-05 15:01:31 +0100

    Changed priority for shortcode auto-injection BEFORE page builders alter content

--- inc/class-content-manager.php
+++ inc/class-content-manager.php
@@ -29,7 +29,7 @@ class Content_Manager implements Hookable {
 		add_action( 'wp_head', array( $this, 'print_schema' ), 10 );
 		add_filter( 'body_class', array( $this, 'body_class' ), 10, 1 );
 		add_filter( 'document_title_parts', array( $this, 'document_title_parts' ), 10, 1 );
-		add_filter( 'the_content', array( $this, 'the_content' ), 10, 2 );
+		add_filter( 'the_content', array( $this, 'the_content' ), 5, 2 );
 		add_filter( 'write_your_story', array( $this, 'write_your_story' ), 10, 2 );
 		add_filter( 'get_pages', array( $this, 'get_front_pages' ), 10, 2 );
 		add_action( 'pre_get_posts', array( $this, 'allow_calendars_front_page' ) );
